2d50aa08d8bb97aeae697b1fc21d23bcd0173f91
Franco Demetrio <francodemetrio87@gmail.com>
2022-05-26 00:51:11 -0300

    Cargando y levantando datos en H2 database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into contactos (nombre, telefono) values ('Silvana Plasencia', '3471472366');
+insert into contactos (nombre, telefono) values ('Francisco Leiva', '3471471458');
+insert into contactos (nombre, telefono) values ('Stella Pinela', '1145879622');
+insert into contactos (nombre, telefono) values ('Daniel Montoya', '341448794');
+insert into contactos (nombre, telefono) values ('Darío Herrera', '3414478963');
+insert into contactos (nombre, telefono) values ('Maximiliano Trotta', '1125487966');
