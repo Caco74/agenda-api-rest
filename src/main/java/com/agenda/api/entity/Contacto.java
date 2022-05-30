8fc66dce898888034bb85d4ea8c5a9ff3c0dba28
Franco Demetrio <francodemetrio87@gmail.com>
2022-05-30 00:38:55 -0300

    Formato de telefono con espacio y guión. Constante tipo Array con codigos de ciudades grandes, 3 digitos.

--- src/main/java/com/agenda/api/entity/Contacto.java
+++ src/main/java/com/agenda/api/entity/Contacto.java
@@ -1,6 +1,10 @@
 package com.agenda.api.entity;
 
+import com.agenda.api.utils.AppConstantes;
+
 import javax.persistence.*;
+
+import java.util.stream.IntStream;
 
 @Entity
 @Table(name = "contactos")
@@ -30,9 +34,7 @@ public class Contacto {
         this.id = id;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
+    public String getNombre() { return nombre; }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -43,6 +45,44 @@ public class Contacto {
     }
 
     public void setTelefono(String telefono) {
-        this.telefono = telefono;
+
+        int prefTresDigitos = Integer.parseInt(telefono.substring(0,3));
+
+        if (telefono.charAt(0) == '1') {
+            this.telefono = "0" + formatoTelefono(telefono, 2, 7);
+        } else if (esCiudadGrande(AppConstantes.CIUDADES_GRANDES, prefTresDigitos)){
+            this.telefono = "0" + formatoTelefono(telefono, 3, 7);
+        } else {
+            this.telefono = "0" + formatoTelefono(telefono, 4, 8);
+        }
+    }
+
+    public String formatoTelefono(String telefono, int espacio, int guion) {
+        StringBuilder formato = new StringBuilder();
+        int digitos = 0;
+
+        for (int i =0; i<telefono.length();i++) {
+            if (digitos==espacio) {
+                formato.append(" ");
+            }
+            formato.append(telefono.charAt(i));
+            digitos++;
+        }
+
+        StringBuilder numero = new StringBuilder();
+        digitos=0;
+
+        for (int i = 0; i<formato.length();i++){
+            if (digitos==guion) {
+                numero.append("-");
+            }
+            numero.append(formato.charAt(i));
+            digitos++;
+        }
+        return numero.toString();
+    }
+
+    public boolean esCiudadGrande(int[] prefijos, int codigoDeArea){
+        return IntStream.of(prefijos).anyMatch(p -> p == codigoDeArea);
     }
 }
